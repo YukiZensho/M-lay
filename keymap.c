a9f54b3b31dad42e686cfc032e138eee94e9c70b
YukiZensho <yukizenshou@gmail.com>
2022-09-04 23:14:25 +0300

    typo

--- keymap.c
+++ keymap.c
@@ -96,7 +96,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
  [_OBAHI] = LAYOUT(
   KC_ESC,          KC_1,         KC_2,         KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,     KC_0,          XP(MACR,TILD),
-  KC_TAB,          XP(ASLW,ASUP),XP(SHLW,SHUP),KC_N,    KC_L,    KC_F,                         KC_R,    KC_S,    KC_T,    KC_W,     XP(THLW,THUP), XP(ACUT,GRAV),
+  KC_TAB,          XP(ASLW,ASUP),XP(SCLW,SCUP),KC_N,    KC_L,    KC_F,                         KC_R,    KC_S,    KC_T,    KC_W,     XP(THLW,THUP), XP(ACUT,GRAV),
   KC_LSFT,         KC_O,         KC_B,         KC_A,    KC_H,    KC_I,                         KC_Y,    KC_E,    KC_U,    KC_M,     KC_J,          XP(NGLW,NGUP),
   KC_LCTRL,        KC_Q,         KC_Z,         KC_D,    KC_G,    KC_P, KC_LOCK,  TG(_THORN),   KC_K,    KC_C,    KC_V,    KC_X,     XP(DHLW,DHUP), XP(SHLW,SHUP),
                                  KC_LALT,      KC_LGUI, MO(_LOWER), KC_SPC, KC_ENT, MO(_RAISE),   KC_BSPC, KC_DEL
